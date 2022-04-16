df3ff7936b65c8204d07fc00443b32fccc6ba603
keattisak55 <78519886+keattisak55@users.noreply.github.com>
2022-04-16 21:11:47 +0700

    update api select img

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AllMovieComponent } from './components/home/all-movie/all-movie.component';
+import { NewMovieComponent } from './components/home/new-movie/new-movie.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AllMovieComponent,
+    NewMovieComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  domainImage:'https://image.tmdb.org/t/p/original'
 };
 
 /*
